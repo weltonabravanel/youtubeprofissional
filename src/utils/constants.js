90371a22ea4e178b73dec2cf345919484910b63b
unknown <tvsilviosantos@gmail.com>
2023-03-15 22:21:57 -0300

    first commit

---
+++ src/utils/constants.js
@@ -0,0 +1,37 @@
+import MusicNoteIcon from "@mui/icons-material/MusicNote";
+import HomeIcon from "@mui/icons-material/Home";
+import OndemandVideoIcon from "@mui/icons-material/OndemandVideo";
+import SportsEsportsIcon from "@mui/icons-material/SportsEsports";
+import LiveTvIcon from "@mui/icons-material/LiveTv";
+import SchoolIcon from "@mui/icons-material/School";
+import FaceRetouchingNaturalIcon from "@mui/icons-material/FaceRetouchingNatural";
+import CheckroomIcon from "@mui/icons-material/Checkroom";
+import GraphicEqIcon from "@mui/icons-material/GraphicEq";
+import TheaterComedyIcon from "@mui/icons-material/TheaterComedy";
+import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
+
+export const logo =
+  "https://logodownload.org/wp-content/uploads/2014/10/youtube-logo-5-2.png";
+
+export const categories = [
+  { name: "Notícias", icon: <HomeIcon /> },
+  { name: "Músicas", icon: <MusicNoteIcon /> },
+  { name: "Educação", icon: <SchoolIcon /> },
+  { name: "Podcast", icon: <GraphicEqIcon /> },
+  { name: "Filmes", icon: <OndemandVideoIcon /> },
+  { name: "Games", icon: <SportsEsportsIcon /> },
+  { name: "Ao vivo", icon: <LiveTvIcon /> },
+  { name: "Esporte", icon: <FitnessCenterIcon /> },
+  { name: "Estilo", icon: <CheckroomIcon /> },
+  { name: "Beleza", icon: <FaceRetouchingNaturalIcon /> },
+  { name: "Comedia", icon: <TheaterComedyIcon /> },
+];
+
+export const demoThumbnailUrl = "https://i.ibb.co/G2L2Gwp/API-Course.png";
+
+export const demoChannelUrl = "/channel/UCxtCt4pX-SHKa0QOtDrlgJw";
+export const demoVideoUrl = "/video/ziNsL4spkPw";
+export const demoChannelTitle = "Khushi Nikam";
+export const demoVideoTitle = "Sample Title";
+export const demoProfilePicture =
+  "http://dergipark.org.tr/assets/app/images/buddy_sample.png";
